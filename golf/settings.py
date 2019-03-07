ff7fd13831c47a57a4d67fc2b161629f4f9568e6
callaway12 <taewankim0925@likelion.org>
2019-03-07 13:04:16 +0900

    '' hosts

--- golf/settings.py
+++ golf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e0kg70jr1ht4fm%c5y#(&9z=28sndy
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
